286f8eb0e786c424105c2c6e93fd76ee98c556ff
Erik Kastrati <erikkastrati1999@gmail.com>
2023-07-12 18:28:48 +0200

    My first project with html and css

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// var tekstet = ["WEB DEVELOPER", "IT CONSULTANT"];
+
+// function ndryshoTekstin() {
+//   var indeksi = 0;
+//   var element = document.getElementById("ndryshoTekstin");
+//   element.innerHTML = tekstet[indeksi];
+//   indeksi++;
+//   if (indeksi >= tekstet.length) {
+//     indeksi = 0;
+//   }
+// }
+// ndryshoTekstin();
+// setInterval(ndryshoTekstin, 4000);
